5f8f152a7a50a19397c43ecb4202c5076aeaff48
iSamBa <bahou.aissam@gmail.com>
2021-03-25 10:57:54 +0100

    fix background image top left

--- dist/main.d9ee62f6.js
+++ dist/main.d9ee62f6.js
@@ -189,7 +189,7 @@ var reloadCSS = require('_css_loader');
 
 module.hot.dispose(reloadCSS);
 module.hot.accept(reloadCSS);
-},{"./..\\..\\images\\bg-pattern-top.svg":[["bg-pattern-top.f25f38c9.svg","images/bg-pattern-top.svg"],"images/bg-pattern-top.svg"],"./..\\..\\images\\bg-pattern-bottom.svg":[["bg-pattern-bottom.11e7f7ba.svg","images/bg-pattern-bottom.svg"],"images/bg-pattern-bottom.svg"],"./..\\..\\images\\bg-pattern-card.svg":[["bg-pattern-card.ac0e14c5.svg","images/bg-pattern-card.svg"],"images/bg-pattern-card.svg"],"_css_loader":"node_modules/parcel-bundler/src/builtins/css-loader.js"}],"node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+},{"./..\\..\\images\\bg-pattern-top.svg":[["bg-pattern-top.f25f38c9.svg","images/bg-pattern-top.svg"],"images/bg-pattern-top.svg"],"./..\\..\\images\\bg-pattern-card.svg":[["bg-pattern-card.ac0e14c5.svg","images/bg-pattern-card.svg"],"images/bg-pattern-card.svg"],"_css_loader":"node_modules/parcel-bundler/src/builtins/css-loader.js"}],"node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
